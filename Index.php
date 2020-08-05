fdad604cbd47f263fa601aecb2248268d96fb1d9
danielvera987 <danielveraangulo703@gmail.com>
2020-08-04 23:27:16 -0500

    se agrego constante de clase por default

--- Index.php
+++ Index.php
@@ -3,33 +3,17 @@ declare(strict_types=1);
 require_once 'vendor/autoload.php';
 require_once "config.php";
 
-if(isset($_GET['c'])){
-  $nombreControlador = $_GET['c'];
+$nombreControlador = (isset($_GET['c'])) ? $_GET['c'] : false;
+$nombreMetodo = (isset($_GET['a'])) ? $_GET['a'] : false;
 
-  if(file_exists("App/Controllers/{$nombreControlador}Controller.php")){
-    $nombreControlador = '\\App\\Controllers\\'.$nombreControlador.'Controller';
-  }
-  else{
-    $nombreControlador = false;
-    error();
-  }
+if($nombreControlador && file_exists("App/Controllers/{$nombreControlador}Controller.php")){
+  $nombreControlador = '\\App\\Controllers\\'.$nombreControlador.'Controller';
 }else{
-  $nombreControlador = '\\App\\Controllers\\DashboardController';
+  $nombreControlador = '\\App\\Controllers\\'.__CLASSDEFAULT__.'Controller';
 }
 
-if(isset($_GET['a'])){
-  $nombreMetodo = $_GET['a'];
-
-  if(method_exists($nombreControlador,$nombreMetodo)){
+if($nombreMetodo && method_exists($nombreControlador,$nombreMetodo)){
     $nombreControlador::$nombreMetodo();
-  }else{
-    ($nombreControlador) ? $nombreControlador::index() : '';
-  }
 }else{
   ($nombreControlador) ? $nombreControlador::index() : '';
-}
-
-function error(){
-  $error = '\\App\\Controllers\\ErrorController';
-  $error::index();
 }
--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-
+define("__CLASSDEFAULT__", "Dashboard");
 define("__CONFIG__", [
   "DB" => [
     "HOST" => "mysql:host=localhost;dbname=minicontable;charset=utf8",
