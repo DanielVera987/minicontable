0298822720dcf54722dc549503030270b5be8884
danielvera987 <danielveraangulo703@gmail.com>
2020-08-05 22:20:44 -0500

    Validato create ingresos

--- Helpers/Validator.php
+++ Helpers/Validator.php
@@ -2,12 +2,20 @@
 namespace Helpers;
 
 use App\Models\Ingreso;
+use Exception;
 
 class Validator
 {
 
   public static function ValidatorIngresoCreate(Ingreso $model)
   {
-
+    try {
+      if(!$model->id || !is_numeric($model->id)) throw new Exception("El id debe ser numero");
+      if(!$model->user_id || !is_numeric($model->user_id)) throw new Exception("El user_id debe ser numero");
+      if(!$model->fecha || !checkdate($model->fecha)) throw new Exception("La fecha debe ser tipo Date");
+    } catch (Exception $th) {
+      return $th;
+    }
+    
   }
 }
