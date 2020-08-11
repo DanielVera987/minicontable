70ba7cdd792e58a37ee9cfb107882425e8ad622b
danielvera987 <danielveraangulo703@gmail.com>
2020-08-10 20:09:03 -0500

    Listado de egresos e ingresos

--- view/egresos/index.php
+++ view/egresos/index.php
@@ -1,5 +1,5 @@
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h1 class="h2">Ingresos</h1>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">Egresos</h1>
       <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group mr-2">
           <button type="button" class="btn btn-sm btn-outline-secondary">Nuevo Egreso</button>
@@ -24,9 +24,9 @@
           </tr>
         </thead>
         <tbody>
-          <?php if(isset($ingresos['error'])): ?>
+          <?php if(isset($egresos['error'])): ?>
             <tr>
-              <td colspan="6" class="text-center"><?= $ingresos['error'] ?></td>
+              <td colspan="6" class="text-center"><?= $egresos['error'] ?></td>
             </tr>
           <?php else: ?>
             <tr>
